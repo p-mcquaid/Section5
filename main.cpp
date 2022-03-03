d377806701efe58afed0b16606beb0536f4b0a7d
p-mcquaid <49034399+p-mcquaid@users.noreply.github.com>
2022-03-03 11:14:09 +0000

    iniy

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1,num2;
+    double num3;
+
+/*
+    cout << "Hello World" << endl;
+
+    cout << "Hello ";
+    cout << "World" << endl;
+
+    cout << "Hello World\n";
+
+*/
+
+  /*   cout << "Enter an integer\n";
+    cin >> num1;
+    cout << "Enter a second integer\n";
+    cin >> num2;
+
+    cout << "You entered " << num1  << " and " << num2 << endl;
+ */
+
+    /* cout << "Enter a double: ";
+    cin >> num3;
+    cout << "You entered: " << num3 << endl;  */
+
+    cout << "Enter a double: ";
+    cin >> num3;
+    cout << "\nEnter an integer: ";
+    cin >> num1;
+    cout << "You entered " << num1 <<" & " << num3 << endl;
+
+
+
+
+     return 0;
+}
